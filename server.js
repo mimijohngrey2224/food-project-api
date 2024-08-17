066a0cf159f3dd07fd92435f0cf0348d9044f095
MIMi <mimijohngrey@outlook.com>
2024-08-17 18:13:24 +0100

    backend

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ dotenv.config();
 connectDB()
 const app = express()
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin:" https://food-project-lac.vercel.app",
     allowedHeaders: ["Content-Type", "Authorization", "token"],
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true
