df0e3d73196141167113985333cb154b0839b683
MIMi <mimijohngrey@outlook.com>
2024-10-23 23:26:11 +0100

    answer

--- server.js
+++ server.js
@@ -16,15 +16,25 @@ const emailRouter = require("./routes/emailRoute");
 
 dotenv.config();
 
-connectDB()
-const app = express()
-app.use(cors({
-    // origin:"http://localhost:5173",
-    origin:"https://food-project-lac.vercel.app",
+// connectDB()
+// const app = express()
+// app.use(cors({
+//     // origin:"http://localhost:5173",
+//     origin:"https://food-project-lac.vercel.app",
+//     allowedHeaders: ["Content-Type", "Authorization", "token"],
+//     methods: ["GET", "POST", "PATCH", "DELETE"],
+//     credentials: true
+// }))
+
+
+const corsOptions = {
+    origin: process.env.FRONTEND_URL, // Set this in your .env
     allowedHeaders: ["Content-Type", "Authorization", "token"],
     methods: ["GET", "POST", "PATCH", "DELETE"],
     credentials: true
-}))
+};
+
+app.use(cors(corsOptions));
 
 
 app.use(express.json())
